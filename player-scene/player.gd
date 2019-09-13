18a301e0e5ff0f2b14c2575ad527bab043d24252
BJNick <>
2019-09-12 19:43:34 -0600

    Player code indent

--- player-scene/player.gd
+++ player-scene/player.gd
@@ -4,7 +4,7 @@ extends RigidBody
 export var speed = 100.0;
 export var mouse_sensitivity = 0.002; # radians/pixel
 
-onready var camera = $Camera
+onready var camera = $Pivot/Camera
 
 func _ready():
 	pass # Replace with function body.
@@ -13,24 +13,24 @@ func _ready():
 #	pass
 
 func get_input():
-    var input_dir = Vector3()
-    if Input.is_action_pressed("move_forward"):
-        input_dir += -camera.global_transform.basis.z
-    if Input.is_action_pressed("move_back"):
-        input_dir += camera.global_transform.basis.z
-    if Input.is_action_pressed("move_left"):
-        input_dir += -camera.global_transform.basis.x
-    if Input.is_action_pressed("move_right"):
-        input_dir += camera.global_transform.basis.x
-    input_dir = input_dir.normalized()
-    return input_dir
-	
+	var input_dir = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		input_dir += -camera.global_transform.basis.z
+	if Input.is_action_pressed("move_back"):
+		input_dir += camera.global_transform.basis.z
+	if Input.is_action_pressed("move_left"):
+		input_dir += -camera.global_transform.basis.x
+	if Input.is_action_pressed("move_right"):
+		input_dir += camera.global_transform.basis.x
+		input_dir = input_dir.normalized()
+	return input_dir
+
 
 func _unhandled_input(event):
-    if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-        rotate_y(-event.relative.x * mouse_sensitivity)
-        $Camera.rotate_x(-event.relative.y * mouse_sensitivity)
-        $Camera.rotation.x = clamp($Camera.rotation.x, -1.2, 1.2)
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
 
 func _physics_process(delta):
 	linear_velocity = get_input() * delta * speed
