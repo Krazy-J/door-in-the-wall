6e4dd319acea32acac6b12777374835ab13e9f7c
BJNick <>
2019-09-13 22:33:19 -0600

    Revert "Door viewport stuff"
    This reverts commit 64cdf61c77eec56ef98d2a90e6402045ed85910e.

--- objects/DoorViewport.gd
+++
@@ -1,8 +0,0 @@
-extends Viewport
-
-func _ready():
-	$Spatial/Camera.make_current()
-	#$DoorCamera.global_transform.origin = Vector3(0, 1, 0)
-	#world = (get_node("/root") as Viewport).world
-	#own_world = true
-	print("camera activated")
