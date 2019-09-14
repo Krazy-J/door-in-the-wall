04b7714a6d788fdf4d6613795e592728a317c4f8
BJNick <>
2019-09-13 23:42:51 -0600

    Doors and cameras ATTEMPT

--- objects/DoorViewport.gd
+++ objects/DoorViewport.gd
@@ -1,8 +1,25 @@
 extends Viewport
 
+onready var exitDoorID = get_parent().exitDoorID
+onready var exitDoor = get_parent().get_parent().get_child(exitDoorID)
+
+onready var player = get_node("/root/Spatial/Player")
+onready var camera = $Spatial/Camera
+
 func _ready():
-	$Spatial/Camera.make_current()
-	#$DoorCamera.global_transform.origin = Vector3(0, 1, 0)
-	#world = (get_node("/root") as Viewport).world
-	#own_world = true
-	print("camera activated")
+	print(exitDoorID)
+	camera.make_current()
+	
+
+func place_camera():
+	#$Spatial/Camera.translation -= get_parent().translation
+	camera.transform = Transform(Basis(), (player as Spatial).transform.origin);
+	camera.translation -= get_parent().translation
+	camera.transform = camera.transform.rotated(Vector3(1, 0, 0), (exitDoor.rotation_degrees.x - get_parent().rotation_degrees.x) / 180 * PI)
+	camera.transform = camera.transform.rotated(Vector3(0, 1, 0), (exitDoor.rotation_degrees.y - get_parent().rotation_degrees.y) / 180 * PI)
+	camera.transform = camera.transform.rotated(Vector3(0, 0, 1), (exitDoor.rotation_degrees.z - get_parent().rotation_degrees.z) / 180 * PI)
+	camera.translation += exitDoor.translation
+
+func _process(delta):
+	place_camera()
+	pass
