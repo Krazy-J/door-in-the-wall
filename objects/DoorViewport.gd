96f391ea678be4bc3d23dafbaecf17f60bc08c19
BJNick <>
2019-09-14 16:56:03 -0600

    Proper door camera rotation

--- objects/DoorViewport.gd
+++ objects/DoorViewport.gd
@@ -9,14 +9,15 @@ func _ready():
 func place_camera(exitDoor):
 	$Spatial.translation = get_node("/root/Main/Player").translation
 	$Spatial.translation -= get_parent().translation
-	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 0, 1), (-get_parent().rotation_degrees.x) / 180 * PI)
+	$Spatial.transform = $Spatial.transform.rotated(Vector3(1, 0, 0), (-get_parent().rotation_degrees.x) / 180 * PI)
 	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 1, 0), (-get_parent().rotation_degrees.y) / 180 * PI)
-	$Spatial.transform = $Spatial.transform.rotated(Vector3(1, 0, 0), (-get_parent().rotation_degrees.z) / 180 * PI)
+	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 0, 1), (-get_parent().rotation_degrees.z) / 180 * PI)
 	$Spatial.translation += Vector3(0, 1, 0)
 	$Spatial.transform = $Spatial.transform.looking_at(Vector3(0, 3, 0), Vector3(0, 1, 0))
-	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 0, 1), (exitDoor.rotation_degrees.x) / 180 * PI)
-	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 1, 0), (180 + exitDoor.rotation_degrees.y) / 180 * PI)
-	$Spatial.transform = $Spatial.transform.rotated(Vector3(1, 0, 0), (exitDoor.rotation_degrees.z) / 180 * PI)
+	$Spatial.transform = $Spatial.transform.rotated(Vector3(1, 0, 0), (exitDoor.rotation_degrees.x) / 180 * PI)
+	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 1, 0), (exitDoor.rotation_degrees.y) / 180 * PI)
+	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 0, 1), (exitDoor.rotation_degrees.z) / 180 * PI)
+	$Spatial.transform = $Spatial.transform.rotated(exitDoor.transform.basis.y, (180) / 180 * PI)
 	$Spatial.translation += exitDoor.translation
 
 func _process(delta):
