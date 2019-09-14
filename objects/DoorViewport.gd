9babbe58565a703db51cb07ccc8bf1f45478f623
Krazy-J <josh@abellsmith.net>
2019-09-14 13:46:56 -0600

    Fixed door cameras and player door
    Fixed the positioning of the cameras used for the door veiwports, and
    made the player door smaller so it isn't right in the player's face.

--- objects/DoorViewport.gd
+++ objects/DoorViewport.gd
@@ -1,24 +1,29 @@
 extends Viewport
 
 onready var exitDoorID = get_parent().exitDoorID
-onready var exitDoor = get_parent().get_parent().get_child(exitDoorID)
 
-onready var player = get_node("/root/Spatial/Player")
+onready var player = get_node("/root/Main/Player")
 onready var camera = $Spatial/Camera
 
 func _ready():
 	print(exitDoorID)
 	camera.make_current()
-	
 
 func place_camera():
+	var exitDoor = get_parent().get_parent().get_child(exitDoorID)
 	#$Spatial/Camera.translation -= get_parent().translation
-	camera.transform = Transform(Basis(), (player as Spatial).transform.origin);
-	camera.translation -= get_parent().translation
-	camera.transform = camera.transform.rotated(Vector3(1, 0, 0), (exitDoor.rotation_degrees.x - get_parent().rotation_degrees.x) / 180 * PI)
-	camera.transform = camera.transform.rotated(Vector3(0, 1, 0), (exitDoor.rotation_degrees.y - get_parent().rotation_degrees.y) / 180 * PI)
-	camera.transform = camera.transform.rotated(Vector3(0, 0, 1), (exitDoor.rotation_degrees.z - get_parent().rotation_degrees.z) / 180 * PI)
-	camera.translation += exitDoor.translation
+	#camera.transform = Transform(Basis(), (player as Spatial).transform.origin);
+	#camera.translation -= get_parent().translation
+	#camera.transform = camera.transform.rotated(Vector3(1, 0, 0), (exitDoor.rotation_degrees.x - get_parent().rotation_degrees.x) / 180 * PI)
+	#camera.transform = camera.transform.rotated(Vector3(0, 1, 0), (exitDoor.rotation_degrees.y - get_parent().rotation_degrees.y) / 180 * PI)
+	#camera.transform = camera.transform.rotated(Vector3(0, 0, 1), (exitDoor.rotation_degrees.z - get_parent().rotation_degrees.z) / 180 * PI)
+	#camera.translation += exitDoor.translation
+	camera.rotation_degrees.x = exitDoor.rotation_degrees.z
+	camera.rotation_degrees.y = exitDoor.rotation_degrees.y
+	camera.rotation_degrees.z = exitDoor.rotation_degrees.x
+	camera.translation = exitDoor.translation
+	camera.rotation_degrees.y += 90
+	camera.translation.y += 3
 
 func _process(delta):
 	place_camera()
