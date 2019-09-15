f2363f928fa0f7b7eab24477d25a4fe7f576b6ee
BJNick <>
2019-09-14 20:37:21 -0600

    Amazing door

--- objects/DoorViewport.gd
+++ objects/DoorViewport.gd
@@ -2,18 +2,20 @@ extends Viewport
 
 onready var camera = $Spatial/Camera
 
+const mipmap_level = 0;
+
 func _ready():
 	print(get_parent().exitDoorID)
 	$Spatial/Camera.make_current()
 
 func place_camera(exitDoor):
-	$Spatial.translation = get_node("/root/Main/Player").translation
+	size = (get_node("/root") as Viewport).size / pow(2, mipmap_level)
+	#$Spatial.translation = get_node("/root/Main/Player").translation
+	$Spatial.global_transform = (get_node("/root/Main/Player/PivotY/PivotX/Camera") as Spatial).global_transform
 	$Spatial.translation -= get_parent().translation
 	$Spatial.transform = $Spatial.transform.rotated(Vector3(1, 0, 0), (-get_parent().rotation_degrees.x) / 180 * PI)
 	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 1, 0), (-get_parent().rotation_degrees.y) / 180 * PI)
 	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 0, 1), (-get_parent().rotation_degrees.z) / 180 * PI)
-	$Spatial.translation += Vector3(0, 1, 0)
-	$Spatial.transform = $Spatial.transform.looking_at(Vector3(0, 3, 0), Vector3(0, 1, 0))
 	$Spatial.transform = $Spatial.transform.rotated(Vector3(1, 0, 0), (exitDoor.rotation_degrees.x) / 180 * PI)
 	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 1, 0), (exitDoor.rotation_degrees.y) / 180 * PI)
 	$Spatial.transform = $Spatial.transform.rotated(Vector3(0, 0, 1), (exitDoor.rotation_degrees.z) / 180 * PI)
