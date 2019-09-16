253f46222b7818f2d2240e62439889d2b973d1da
Abell-Smith3903032 <3903032@sps-student.sk.ca>
2019-09-16 14:13:00 -0600

    Finalized door teleport
    Finally made the door transformation work properly, once and for all.

--- player/Player.gd
+++ player/Player.gd
@@ -28,7 +28,7 @@ func get_input():
 		input_dir -= camera_g_basis.x
 	if Input.is_action_pressed("move_right"):
 		input_dir += camera_g_basis.x
-	input_dir = (input_dir - input_dir * transform.basis.y).normalized()
+	input_dir = (input_dir - input_dir * Vector3(abs(transform.basis.y.x), abs(transform.basis.y.y), abs(transform.basis.y.z))).normalized()
 	
 	return input_dir
 
@@ -43,7 +43,7 @@ func _physics_process(delta):
 	linear_velocity = get_input() * delta * speed
 	add_central_force(-transform.basis.y * delta * gravity)
 	if Input.is_action_just_pressed("jump"):
-		jump += jump_power
+		jump = jump_power
 	if jump > 0:
 		add_central_force(transform.basis.y * delta * jump)
 		jump -= 1000
