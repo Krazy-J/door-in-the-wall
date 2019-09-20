8250cf7452075281aa26cca44905125b3a47a389
Abell-Smith3903032 <3903032@sps-student.sk.ca>
2019-09-20 15:31:08 -0600

    Door interaction progress
    Working on door interaction capabilities.

--- objects/DoorOpen.gd
+++ objects/DoorOpen.gd
@@ -1,9 +1,29 @@
-extends StaticBody
+extends Area
 
 var open = false
+var selected = false
 
-func _ready():
+func toggle_door():
 	if open:
-		get_node("AnimationPlayer").play("DoorOpen")
+		if self.exitDoorID % 2:
+			get_node("AnimationPlayer").play_backwards("DoorOpen-")
+		else:
+			get_node("AnimationPlayer").play_backwards("DoorOpen+")
 	else:
-		get_node("AnimationPlayer").play_backwards("DoorOpen")
+		if self.exitDoorID % 2:
+			get_node("AnimationPlayer").play("DoorOpen-")
+		else:
+			get_node("AnimationPlayer").play("DoorOpen+")
+
+func _on_Door_area_entered(area):
+	if area.name == "InteractRay":
+		selected = true
+
+func _on_Door_area_exited(area):
+	if area.name == "InteractRay":
+		selected = false
+
+func _process(delta):
+	if selected and Input.is_action_just_pressed("interact"):
+		get_parent().toggle_door()
+		get_parent().get_parent().get_child(get_parent().exitDoorID).toggle_door()
--- player/Player.gd
+++ player/Player.gd
@@ -1,11 +1,10 @@
 extends KinematicBody
 
-export var speed = 100
+export var speed = 60
 export var jump_power = 25
 export var gravity = 80
 export var mouse_sensitivity = 0.003 # radians/pixel
 var motion = Vector3()
-var movement = Vector3()
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -17,19 +16,23 @@ func _input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func get_input():
-	var input_dir = Vector3()
+	var movement = Vector3()
 	var camera_g_basis = $PivotY.transform.basis
 	if Input.is_action_pressed("move_forward"):
-		input_dir -= camera_g_basis.z
+		movement -= camera_g_basis.z
 	if Input.is_action_pressed("move_back"):
-		input_dir += camera_g_basis.z
+		movement += camera_g_basis.z
 	if Input.is_action_pressed("move_left"):
-		input_dir -= camera_g_basis.x
+		movement -= camera_g_basis.x
 	if Input.is_action_pressed("move_right"):
-		input_dir += camera_g_basis.x
+		movement += camera_g_basis.x
 	#input_dir = (input_dir - input_dir * Vector3(abs(transform.basis.y.x), abs(transform.basis.y.y), abs(transform.basis.y.z))).normalized()
-	input_dir = Vector3(input_dir.x, 0, input_dir.z).normalized()
-	return input_dir
+	movement = Vector3(movement.x, 0, movement.z).normalized()
+	if Input.is_action_pressed("sprint"):
+		movement *= 2
+	if !get_node("GroundRay").is_colliding():
+		movement /= 20
+	return movement
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -39,15 +42,12 @@ func _unhandled_input(event):
 		$PivotY/PivotX.rotation.x = clamp($PivotY/PivotX.rotation.x, -1.5, 1.5)
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("jump") and get_node("GroundRay").is_colliding():
-		motion.y += jump_power
-	else: if get_node("GroundRay").is_colliding():
-		motion += get_input() * speed * delta
-		if motion.y < 0:
-			motion.y = 0
+	motion += global_transform.basis * get_input() * speed * delta
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			motion += jump_power * transform.basis.y
 		motion /= 1.2
 	else:
-		motion += get_input() * speed * delta / 20
-		motion.y -= gravity * delta
+		motion -= gravity * delta * transform.basis.y
 		motion /= 1.01
-	movement = move_and_slide(global_transform.basis * motion, Vector3(0, -1, 0))
+	motion = move_and_slide(motion, transform.basis.y)
