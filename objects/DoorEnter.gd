c6ee973d732acffd9117eb3456a96fefd9428e21
Krazy-J <josh@abellsmith.net>
2019-09-19 20:21:26 -0600

    Player movement
    Improved player movement some more.

--- objects/DoorEnter.gd
+++ objects/DoorEnter.gd
@@ -6,5 +6,5 @@ func _on_DoorEnter_body_entered(body):
 	if body.name == "Player":
 		var exitDoor = get_parent().get_child(exitDoorID)
 		body.transform = self.transform.inverse() * body.transform
-		body.transform = body.transform.rotated(Vector3(0, 1, 0), PI)
+		body.transform = body.transform.rotated(Vector3(0, -1, 0), PI)
 		body.transform = exitDoor.transform * body.transform
