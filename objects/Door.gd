8f0415c723fb954b8a3cfa37c5845411c9412f49
Krazy-J <josh@abellsmith.net>
2019-09-23 19:34:27 -0600

    Combined door scripts
    Combined the DoorEnter, DoorOpen, and DoorViewport scripts into a Door
    script.

---
+++ objects/Door.gd
@@ -0,0 +1,52 @@
+extends MeshInstance
+
+export var exitDoorName = String(0)
+var open = false
+var selected = false
+var firstDoor = false
+const mipmap_level = 0
+
+func _ready():
+	print(exitDoorName)
+	$Viewport/Spatial/Camera.make_current()
+
+func _on_DoorEnter_body_entered(body):
+	if body.name == "Player":
+		var exitDoor = get_parent().get_node("./" + exitDoorName)
+		body.transform = self.transform.inverse() * body.transform
+		body.transform = body.transform.rotated(Vector3(0, 1, 0), PI)
+		body.transform = exitDoor.transform * body.transform
+		body.motion = self.global_transform.basis.inverse() * body.motion
+		body.motion = body.motion.rotated(Vector3(0, 1, 0), PI)
+		body.motion = exitDoor.global_transform.basis * body.motion
+
+func place_camera(exitDoor):
+	$Viewport.size = (get_node("/root") as Viewport).size / pow(2, mipmap_level)
+	$Viewport/Spatial.global_transform = (get_node("/root/Main/ViewportContainer/PlayerViewport/Player/PivotY/PivotX/Camera") as Spatial).global_transform
+	$Viewport/Spatial.transform = self.transform.inverse() * $Viewport/Spatial.transform
+	$Viewport/Spatial.transform = $Viewport/Spatial.transform.rotated(Vector3(0, 1, 0), PI)
+	$Viewport/Spatial.transform = exitDoor.transform * $Viewport/Spatial.transform
+
+func toggle_door():
+	open = !open
+	if open:
+		return $Door/AnimationPlayer.play("DoorToggle" + String(firstDoor))
+	else:
+		return $Door/AnimationPlayer.play_backwards("DoorToggle" + String(firstDoor))
+	print(String(firstDoor))
+
+func _on_Door_area_entered(area):
+	if area.name == "InteractArea":
+		selected = true
+
+func _on_Door_area_exited(area):
+	if area.name == "InteractArea":
+		selected = false
+
+func _process(delta):
+	firstDoor = !get_parent().get_node("./" + exitDoorName).firstDoor
+	if selected and Input.is_action_just_pressed("interact"):
+		toggle_door()
+		get_parent().get_node("./" + exitDoorName).toggle_door()
+	place_camera(get_parent().get_node("./" + exitDoorName))
+	pass
