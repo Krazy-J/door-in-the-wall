ede971fed08485b874ca2a52fa3a098a4ba409c0
Abell-Smith3903032 <3903032@sps-student.sk.ca>
2019-10-21 08:18:06 -0600

    Door sounds
    Made door sounds line up better with the animation. Also renamed some files.

--- door/Door.gd
+++ door/Door.gd
@@ -13,21 +13,20 @@ func toggle_door():
 	open = !open
 	if open:
 		$Door/AnimationPlayer.play("DoorToggle")
-		$Door/SoundOpen.play()
+		$Door/SoundClose.stop()
+		$Door/SoundOpen.play($Door/AnimationPlayer.current_animation_position)
 	else:
 		$Door/AnimationPlayer.play_backwards("DoorToggle")
-		$Door/SoundClose.play()
+		$Door/SoundOpen.stop()
+		$Door/SoundClose.play(1 - $Door/AnimationPlayer.current_animation_position)
 
 func _on_Interact_area_entered(area):
-	if area.name == "InteractArea":
-		selected = true
+	if area.name == "InteractArea": selected = true
 
 func _on_Interact_area_exited(area):
-	if area.name == "InteractArea":
-		selected = false
+	if area.name == "InteractArea": selected = false
 
 func _process(delta):
 	if selected and Input.is_action_just_pressed("interact") and $Door:
 		toggle_door()
-		if $Connection:
-			get_parent().get_node("./" + exitDoorName).toggle_door()
+		if $Connection: get_parent().get_node("./" + exitDoorName).toggle_door()
