cfd0db4a427b6fc2a3d16de2d5acb0ce3d7754e2
Krazy-J <53985555+Krazy-J@users.noreply.github.com>
2019-11-06 16:33:56 -0600

    Hinge again
    Changed the hinge again, it's entirely different. It moves properly.

---
+++ door/hinged/hinge.gd
@@ -0,0 +1,5 @@
+extends MeshInstance
+
+export var hinge_position = Vector3(.125, 0, 1.5)
+
+func _process(delta): transform.origin = hinge_position - hinge_position.rotated(transform.basis.y, rotation_degrees.y / 180 * PI)
