ab9a3a281c94c1d9130075621478879bd25b8a43
Krazy-J <53985555+Krazy-J@users.noreply.github.com>
2019-11-12 15:40:11 -0600

    Tweaks

--- object/block/block.gd
+++ object/block/block.gd
@@ -2,5 +2,9 @@ extends RigidBody
 
 func _input(event):
 	if event.is_action_pressed("interact"):
-		if $"/root/Player".carrying == get_path(): $"/root/Player".carrying = NodePath()
-		elif $Interact.visible and !$"/root/Player".carrying: $"/root/Player".carrying = get_path()
+		if $"/root/Player".carrying == get_path():
+			$"/root/Player".carrying = NodePath()
+			$"/root/Player".carry_distance = float()
+		elif $Interact.visible and !$"/root/Player".carrying:
+			$"/root/Player".carrying = get_path()
+			$"/root/Player".carry_distance = global_transform.origin.distance_to($"/root/Player/PivotX".global_transform.origin)
--- player/player.gd
+++ player/player.gd
@@ -7,6 +7,7 @@ export var surface_friction = .2
 export var gravity = 80
 export var mouse_sensitivity = .003 # radians/pixel
 export var carrying : NodePath
+export var carry_distance : float
 var motion : Vector3
 
 func _ready():
@@ -16,12 +17,12 @@ func _unhandled_input(event):
 	if event.is_action_pressed("left_mouse"): Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if event.is_action_pressed("exit_mouse"): Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotate(transform.basis.y.normalized(), -event.relative.x * mouse_sensitivity)
+		rotate(global_transform.basis.y.normalized(), -event.relative.x * mouse_sensitivity)
 		$PivotX.rotate_x(-event.relative.y * mouse_sensitivity)
 		$PivotX.rotation.x = clamp($PivotX.rotation.x, -1.5, 1.5)
 	if event.is_action_pressed("exit_mouse"): Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if is_on_floor():
-		if event.is_action_pressed("jump"): motion += transform.basis.y * jump_power
+		if event.is_action_pressed("jump"): motion += global_transform.basis.y * jump_power
 
 func get_movement():
 	var movement : Vector3
@@ -49,11 +50,11 @@ func _physics_process(delta):
 			body.get_node("Door").rotation_degrees += ($CarryDoor.rotation_degrees - body.get_node("Door").rotation_degrees) * 10 * delta
 			body.get_node("Door").scale += ($CarryDoor.scale - body.get_node("Door").scale) * 10 * delta
 		else:
-			body.linear_velocity = ($PivotX.global_transform.origin - $PivotX.global_transform.basis.z * 3 - body.global_transform.origin) * 500 * delta
+			body.linear_velocity = ($PivotX.global_transform.origin - $PivotX.global_transform.basis.z.normalized() * carry_distance - body.global_transform.origin) * 500 * delta
 			body.angular_velocity = -body.rotation_degrees * 20 * delta
 	motion += transform.basis * get_movement() * speed * delta
 	if is_on_floor():
 		motion *= (1 - surface_friction)
 	motion *= (1 - air_resistance)
-	motion -= transform.basis.y * gravity * delta
-	motion = move_and_slide(motion, transform.basis.y.normalized())
+	motion -= global_transform.basis.y * gravity * delta
+	motion = move_and_slide(motion, global_transform.basis.y.normalized())
