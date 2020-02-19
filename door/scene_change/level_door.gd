c866fb5556cd2b01700d00ca8417bab85ce162c1
Krazy-J <josh@abellsmith.net>
2020-02-18 20:27:58 -0600

    Removed GoalDoor.tscn
    Changed the level door to have a color variable for the label, making the goal door scene unnecessary. It is instead just a lobby door with different variables.

--- door/scene_change/level_door.gd
+++ door/scene_change/level_door.gd
@@ -3,9 +3,11 @@ extends "res://door/door.gd"
 
 export(PackedScene) var room
 export(String, MULTILINE) var label
+export var label_color : Color
 
 func _ready():
 	$Label.text = label
+	$Label.color = label_color
 
 func load_fade(): get_viewport().call_deferred("add_child", load("res://interface/Fade.tscn").instance())
 # warning-ignore:return_value_discarded
@@ -14,3 +16,4 @@ func load_scene(): get_tree().change_scene(room.resource_path)
 func _process(_delta):
 	if Engine.editor_hint:
 		$Label.text = label
+		$Label.color = label_color
