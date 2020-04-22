688e07823aff0a4d3269c7f64691fcc61c18b6d0
Krazy-J <josh@abellsmith.net>
2020-04-21 19:49:38 -0600

    Desk Lamp Fixes
    Made Desk Lamp affected by Object Models Quality setting, and fixed some minor bugs.

--- player/player.gd
+++ player/player.gd
@@ -33,27 +33,28 @@ func _on_quit_game(): get_tree().quit()
 func _unhandled_input(event):
 	if event.is_action_pressed("exit_mouse"): $Pause.popup()
 	if event.is_action_pressed("fullscreen"): OS.window_fullscreen = not OS.window_fullscreen
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			if Input.is_mouse_button_pressed(BUTTON_MIDDLE) and carrying:
-				var body = carrying
-				body.rotate(transform.basis.y.normalized(), -event.relative.x * ProjectSettings.mouse_sensitivity)
-				if not body.is_class("StaticBody"): body.rotate(transform.basis.x.normalized(), -event.relative.y * ProjectSettings.mouse_sensitivity)
-			else:
+	if not $Pause.visible:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			if event is InputEventMouseMotion:
+				if Input.is_mouse_button_pressed(BUTTON_MIDDLE) and carrying:
+					var body = carrying
+					body.rotate(transform.basis.y.normalized(), -event.relative.x * ProjectSettings.mouse_sensitivity)
+					if not body.is_class("StaticBody"): body.rotate(transform.basis.x.normalized(), -event.relative.y * ProjectSettings.mouse_sensitivity)
+				else:
+					rotate(transform.basis.y.normalized(), -event.relative.x * ProjectSettings.mouse_sensitivity)
+					$PivotX.rotate_x(-event.relative.y * ProjectSettings.mouse_sensitivity)
+					$PivotX.rotation.x = clamp($PivotX.rotation.x, -1.5, 1.5)
+			if event is InputEventJoypadMotion:
 				rotate(transform.basis.y.normalized(), -event.relative.x * ProjectSettings.mouse_sensitivity)
 				$PivotX.rotate_x(-event.relative.y * ProjectSettings.mouse_sensitivity)
 				$PivotX.rotation.x = clamp($PivotX.rotation.x, -1.5, 1.5)
-		if event is InputEventJoypadMotion:
-			rotate(transform.basis.y.normalized(), -event.relative.x * ProjectSettings.mouse_sensitivity)
-			$PivotX.rotate_x(-event.relative.y * ProjectSettings.mouse_sensitivity)
-			$PivotX.rotation.x = clamp($PivotX.rotation.x, -1.5, 1.5)
-	if carrying and event is InputEventMouseButton:
-		if Input.is_mouse_button_pressed(BUTTON_WHEEL_UP):
-			carry_distance = min(carry_distance + .1, abs($PivotX/Interact.cast_to.z))
-		if Input.is_mouse_button_pressed(BUTTON_WHEEL_DOWN):
-			carry_distance = max(carry_distance - .1, 2)
-	if is_on_floor():
-		if event.is_action("jump"): motion += transform.basis.y * jump_power
+		if carrying and event is InputEventMouseButton:
+			if Input.is_mouse_button_pressed(BUTTON_WHEEL_UP):
+				carry_distance = min(carry_distance + .1, abs($PivotX/Interact.cast_to.z))
+			if Input.is_mouse_button_pressed(BUTTON_WHEEL_DOWN):
+				carry_distance = max(carry_distance - .1, 2)
+		if is_on_floor():
+			if event.is_action("jump"): motion += transform.basis.y * jump_power
 
 func get_movement():
 	var movement : Vector3
@@ -81,27 +82,28 @@ func _process(_delta):
 			interact.interact_entered()
 
 func _physics_process(delta):
-	if carrying:
-		if carrying.has_node("Door"):
-			carrying.global_transform.origin += (transform.origin - carrying.global_transform.origin) * 10 * delta
-			carrying.get_node("Door").translation += ($CarryDoor.translation / carrying.scale * scale - carrying.get_node("Door").scale * Vector3(0, 3.5, 0) - carrying.get_node("Door").translation) * 10 * delta
-			if (transform.basis * (rotation_degrees - carrying.rotation_degrees)).y > 180:
-				carrying.rotation_degrees += (rotation_degrees - carrying.rotation_degrees - transform.basis.y * 360) * 10 * delta
-			elif (transform.basis * (rotation_degrees - carrying.rotation_degrees)).y < -180:
-				carrying.rotation_degrees += (rotation_degrees - carrying.rotation_degrees + transform.basis.y * 360) * 10 * delta
+	if not $Pause.visible:
+		if carrying:
+			if carrying.has_node("Door"):
+				carrying.global_transform.origin += (transform.origin - carrying.global_transform.origin) * 10 * delta
+				carrying.get_node("Door").translation += ($CarryDoor.translation / carrying.scale * scale - carrying.get_node("Door").scale * Vector3(0, 3.5, 0) - carrying.get_node("Door").translation) * 10 * delta
+				if (transform.basis * (rotation_degrees - carrying.rotation_degrees)).y > 180:
+					carrying.rotation_degrees += (rotation_degrees - carrying.rotation_degrees - transform.basis.y * 360) * 10 * delta
+				elif (transform.basis * (rotation_degrees - carrying.rotation_degrees)).y < -180:
+					carrying.rotation_degrees += (rotation_degrees - carrying.rotation_degrees + transform.basis.y * 360) * 10 * delta
+				else:
+					carrying.rotation_degrees += (rotation_degrees - carrying.rotation_degrees) * 10 * delta
+				carrying.get_node("Door").rotation_degrees += ($CarryDoor.rotation_degrees - carrying.get_node("Door").rotation_degrees) * 10 * delta
+				carrying.get_node("Door").scale += ($CarryDoor.scale - carrying.get_node("Door").scale) * 10 * delta
+			elif carrying.is_class("StaticBody"):
+				carrying.constant_linear_velocity = ($PivotX.global_transform.origin - $PivotX.global_transform.basis.z.normalized() * carry_distance - carrying.global_transform.origin) * 500 * delta
+			elif carrying.is_class("RigidBody"):
+				carrying.linear_velocity = ($PivotX.global_transform.origin - $PivotX.global_transform.basis.z.normalized() * carry_distance - carrying.global_transform.origin) * 500 * delta
 			else:
-				carrying.rotation_degrees += (rotation_degrees - carrying.rotation_degrees) * 10 * delta
-			carrying.get_node("Door").rotation_degrees += ($CarryDoor.rotation_degrees - carrying.get_node("Door").rotation_degrees) * 10 * delta
-			carrying.get_node("Door").scale += ($CarryDoor.scale - carrying.get_node("Door").scale) * 10 * delta
-		elif carrying.is_class("StaticBody"):
-			carrying.constant_linear_velocity = ($PivotX.global_transform.origin - $PivotX.global_transform.basis.z.normalized() * carry_distance - carrying.global_transform.origin) * 500 * delta
-		elif carrying.is_class("RigidBody"):
-			carrying.linear_velocity = ($PivotX.global_transform.origin - $PivotX.global_transform.basis.z.normalized() * carry_distance - carrying.global_transform.origin) * 500 * delta
-		else:
-			carrying.global_transform = $PivotX/Carry.global_transform
-	motion += transform.basis * get_movement() * speed * delta
-	if is_on_floor():
-		motion *= (1 - surface_friction)
-	motion *= (1 - air_resistance)
-	motion -= transform.basis.y * gravity * delta
-	motion = move_and_slide(motion, transform.basis.y.normalized())
+				carrying.global_transform = $PivotX/Carry.global_transform
+		motion += transform.basis * get_movement() * speed * delta
+		if is_on_floor():
+			motion *= (1 - surface_friction)
+		motion *= (1 - air_resistance)
+		motion -= transform.basis.y * gravity * delta
+		motion = move_and_slide(motion, transform.basis.y.normalized())
